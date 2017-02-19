867a0fce6e1f14598ee1219a7e0782635c663e43
gp22 <gp22@tutanota.com>
2017-02-19 10:59:59 +0700

    add pretty urls using html5mode

--- app/app.config.js
+++ app/app.config.js
@@ -10,5 +10,8 @@ angular.
                 when('/polls/new', {
                     template: '<new-poll-form></new-poll-form>'
                 })
+
+             // use the HTML5 History API
+            $locationProvider.html5Mode(true);
         }
     ]);
