74f7b30622bd52d0821d1459cf8ab11916dde35a
gp22 <gp22@tutanota.com>
2017-02-20 14:31:17 +0700

    change createPoll() to updatePoll()

--- app/new-poll-form/new-poll-form.component.js
+++ app/new-poll-form/new-poll-form.component.js
@@ -21,7 +21,7 @@ angular
             };
 
             // send poll data to server.js
-            this.createPoll = () => {
+            this.updatePoll = () => {
                 const poll = this.poll;
                 const options = poll.options;
                 const length = options.length;
