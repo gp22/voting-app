538c2b46e8b3929ec9a575de7a1a42457a45cc4c
gp22 <gp22@tutanota.com>
2017-02-27 13:43:29 +0700

    add login and signup routes and forms

--- app/app.config.js
+++ app/app.config.js
@@ -10,20 +10,25 @@ angular
                 .when('/polls', {
                     template: '<show-polls></show-polls>'
                 })
-
                 // NEW route
                 .when('/polls/new', {
                     template: '<new-poll-form></new-poll-form>'
                 })
-
                 // SHOW route
                 .when('/polls/:id', {
                     template: '<show-poll></show-poll>'
                 })
-
                 // EDIT route
                 .when('/polls/:id/edit', {
                     template: '<edit-poll-form></edit-poll-form>'
+                })
+                // SIGNUP route
+                .when('/signup', {
+                    template: '<signup-form></signup-form>'
+                })
+                // LOGIN route
+                .when('/login', {
+                    template: '<login-form></login-form>'
                 })
 
              // use the HTML5 History API
