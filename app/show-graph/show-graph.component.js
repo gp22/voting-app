834f8422df27a6bf25b0fd3263565fb7ef97985f
gp22 <gp22@tutanota.com>
2017-03-10 11:27:52 +0700

    add doughnut chart

--- app/show-graph/show-graph.component.js
+++ app/show-graph/show-graph.component.js
@@ -7,22 +7,55 @@ angular
         templateUrl: '/show-graph/show-graph.template.html',
         controller: function showGraphController($routeParams, $http) {
 
-            // define the empty poll object to save the poll
-            // data returned from the database
-            this.poll = {};
-
             // Get the poll to show from the database
             $http.get(`/api/polls/${$routeParams.id}`).then(res => {
                 this.poll = res.data;
+
+                let labels = [];
+                let data = [];
+
+                this.poll.options.forEach(option => {
+                    labels.push(option.name);
+                    data.push(option.score);
+                });
+
+                // console.log(labels, data);
+
+                const ctx = document.getElementById("pollChart");
+                const pollChart = new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: labels,
+                        // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: data,
+                            // data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        maintainAspectRatio: false
+                    }
+                });
+
             });
 
-            // this.polls = {};
-            // // const username = Auth.currentUser();
-            // this.username = Auth.currentUser();
-
-            // // Get the poll to show from the database
-            // $http.get(`/api/users/${this.username}/polls`).then(res => {
-            //     this.polls = res.data;
-            // });
         }
     });
