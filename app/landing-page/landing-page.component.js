1246ef5a905958410fa5f199cfee47c38e87cd82
gp22 <gp22@tutanota.com>
2017-03-13 15:55:08 +0700

    add styling

--- app/landing-page/landing-page.component.js
+++ app/landing-page/landing-page.component.js
@@ -6,27 +6,5 @@ angular
     .component('landingPage', {
         templateUrl: '/landing-page/landing-page.template.html',
         controller: function landingPageController($http, $window, $location) {
-            // this.user = {
-            //     username: '',
-            //     password: ''
-            // };
-
-            // send user data to server.js
-            // this.login = () => {
-            //     const user = this.user;
-
-            //     // send user to server.js if no fields were left empty
-            //     if (user.username != '' &&
-            //         user.password != '') {
-            //         $http.post('/api/login', user).then(res => {
-            //             // save the JSON token in the response to local storage
-            //             $window.localStorage.token = res.data.token;
-            //             // redirect to their profile page
-            //             $location.url('/profile');
-            //         }, res => {
-            //             $location.url('/');
-            //         });
-            //     }
-            // };
         }
     });
--- app/show-user-polls/show-user-polls.component.js
+++ app/show-user-polls/show-user-polls.component.js
@@ -7,13 +7,19 @@ angular
         templateUrl: '/show-user-polls/show-user-polls.template.html',
         controller: function showPollsController($http, Auth) {
 
-            this.polls = {};
+            // this.polls = {};
             // const username = Auth.currentUser();
             this.username = Auth.currentUser();
+
+            // create the app object for displaying error messages
+            let app = this;
 
             // Get the poll to show from the database
             $http.get(`/api/users/${this.username}/polls`).then(res => {
                 this.polls = res.data;
+                if (this.polls.length === 0) {
+                    app.errorMsg = 'You don\'t have any polls.';
+                }
             });
         }
     });
