45b9732033b7a5199120acc74def1f231d00daff
gp22 <gp22@tutanota.com>
2017-03-21 11:02:15 +0700

    add token to Authorization headers

--- app/show-user-polls/show-user-polls.component.js
+++ app/show-user-polls/show-user-polls.component.js
@@ -5,7 +5,7 @@ angular
     .module('showUserPolls')
     .component('showUserPolls', {
         templateUrl: '/show-user-polls/show-user-polls.template.html',
-        controller: function showPollsController($http, Auth) {
+        controller: function showPollsController($http, $window, Auth) {
 
             // this.polls = {};
             // const username = Auth.currentUser();
@@ -14,8 +14,17 @@ angular
             // create the app object for displaying error messages
             let app = this;
 
+            // store the token for sending in the Authorization header
+            const token = $window.localStorage.token;
+
             // Get the poll to show from the database
-            $http.get(`/api/users/${this.username}/polls`).then(res => {
+            $http.get(`/api/users/${this.username}/polls`, {
+                // add the token to the Authorization header
+                // to get access to the protected express route
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            }).then(res => {
                 this.polls = res.data;
                 if (this.polls.length === 0) {
                     app.errorMsg = 'You don\'t have any polls.';
